41236c662b5f6863bac232baa9c488d2d247c519
jhenriquedm <jhenriquedm98@gmail.com>
2026-05-29 19:00:13 +0000

    Configuração Firebase no projeto Flutter

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
 import 'views/auth/login_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: MyApp(),
